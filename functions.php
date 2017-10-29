95f4750dac60377279c5bdf5eace672e0ba441e6
Peace <shantipaija.com.np@gmail.com>
2017-10-29 20:28:32 +0545

    footer widget and color update

--- functions.php
+++ functions.php
@@ -174,47 +174,9 @@ function peace_widgets_init() {
 
 	register_sidebar(array(
 		'id'            => 'footer-widget-1',
-		'name'          => esc_html__( 'Footer Widget 1', 'peace' ),
+		'name'          => esc_html__( 'Footer Widgets', 'peace' ),
 		'description'   => esc_html__( 'Used for footer widget area', 'peace' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widgettitle">',
-		'after_title'   => '</h3>',
-	));
-
-	register_sidebar(array(
-		'id'            => 'footer-widget-2',
-		'name'          => esc_html__( 'Footer Widget 2', 'peace' ),
-		'description'   => esc_html__( 'Used for footer widget area', 'peace' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widgettitle">',
-		'after_title'   => '</h3>',
-	));
-
-	register_sidebar(array(
-		'id'            => 'footer-widget-3',
-		'name'          => esc_html__( 'Footer Widget 3', 'peace' ),
-		'description'   => esc_html__( 'Used for footer widget area', 'peace' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widgettitle">',
-		'after_title'   => '</h3>',
-	));
-	register_sidebar(array(
-		'id'            => 'footer-widget-4',
-		'name'          => esc_html__( 'Footer Widget 4', 'peace' ),
-		'description'   => esc_html__( 'Used for footer widget area', 'peace' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widgettitle">',
-		'after_title'   => '</h3>',
-	));
-	register_sidebar(array(
-		'id'            => 'footer-widget-5',
-		'name'          => esc_html__( 'Footer Widget 5', 'peace' ),
-		'description'   => esc_html__( 'Used for footer widget area', 'peace' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'before_widget' => '<div id="%1$s" class="widget col-sm-3 %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="widgettitle">',
 		'after_title'   => '</h3>',
