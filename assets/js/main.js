367d32f3a3778e95755d5c2fd34e967feac76ff1
AnupRaj <anupofuniverse@gmail.com>
2017-11-15 16:16:13 +0545

    replaced $ to jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,22 @@
   jQuery('.zoom-thumbnail-tile')
     // tiles mouse actions
     .on('mouseover', function(){
-      $(this).children('.zoom-thumbnail-photo').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
+      jQuery(this).children('.zoom-thumbnail-photo').css({'transform': 'scale('+ jQuery(this).attr('data-scale') +')'});
     })
     .on('mouseout', function(){
-      $(this).children('.zoom-thumbnail-photo').css({'transform': 'scale(1)'});
+      jQuery(this).children('.zoom-thumbnail-photo').css({'transform': 'scale(1)'});
     })
     .on('mousemove', function(e){
-      $(this).children('.zoom-thumbnail-photo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+      jQuery(this).children('.zoom-thumbnail-photo').css({'transform-origin': ((e.pageX - jQuery(this).offset().left) / jQuery(this).width()) * 100 + '% ' + ((e.pageY - jQuery(this).offset().top) / jQuery(this).height()) * 100 +'%'});
     })
 
     // tiles set up
     .each(function(){
-      $(this)
+      jQuery(this)
         // add a photo container
         .append('<div class="zoom-thumbnail-photo single-featured"></div>')
         // some text just to show zoom level on current item in this example
-        // .append('<div class="txt"><div class="x">'+ $(this).attr('data-scale') +'x</div>ZOOM ON<br>HOVER</div>')
+        // .append('<div class="txt"><div class="x">'+ jQuery(this).attr('data-scale') +'x</div>ZOOM ON<br>HOVER</div>')
         // set up a background image for each tile based on data-image attribute
-        .children('.zoom-thumbnail-photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
+        .children('.zoom-thumbnail-photo').css({'background-image': 'url('+ jQuery(this).attr('data-image') +')'});
     })
