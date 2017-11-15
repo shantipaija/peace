3c9fcee937361a973d413a3745870bd8d984b62b
AnupRaj <anupofuniverse@gmail.com>
2017-11-15 16:04:15 +0545

    single !!  is fullwidth page ?

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,20 +1,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="imghov">
-	<?php
-	/*
-	if ( is_page_template( 'page-fullwidth.php' ) ) {
-		the_post_thumbnail( 'peace-featured-fullwidth',  array(
-		'class' => 'single-featured fullwidth',
-	) );
-	} else {
-			
-		the_post_thumbnail( 'peace-featured', array(
-			'class' => 'single-featured sidebars',
-		) );
-	}*/
-	?>
 		<div class="tiles">
-		    <div class="zoom-thumbnail-tile" data-scale="1.6" data-image="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>"></div>
+			<div 
+			 data-scale="1.6" 
+			 data-image="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" 
+			 class="zoom-thumbnail-tile <?php 
+			if ( is_page_template( 'page-fullwidth.php' ) ) {
+				echo "single-featured fullwidth";
+			} else {
+				echo "single-featured sidebars";
+			} 
+			?>"></div>
 		</div>
 	</div>
 	<div class="post-inner-content">
