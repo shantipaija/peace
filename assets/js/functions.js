7dfe8904dce1b8e9fa6efbe839d5ffaaadecacda
Anup Raj <anupofuniverse@gmail.com>
2017-12-20 12:50:15 +0545

    js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -106,3 +106,25 @@ jQuery( document ).ready(function() {
         .children('.zoom-thumbnail-tile').css({'background-image': 'url('+ jQuery(this).attr('data-image') +')'});
     });
 });
+
+//# sourceMappingURL=skip-link-focus-fix.min.js.map
+( function() {// jscs:ignore validateLineBreaks
+	var isWebkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    isOpera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    isIE     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1,
+        eventMethod;
+
+	if ( ( isWebkit || isOpera || isIE ) && 'undefined' !== typeof( document.getElementById ) ) {
+		eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+                    element.tabIndex = -1;
+                }
+				element.focus();
+			}
+		}, false );
+	}
+})();
