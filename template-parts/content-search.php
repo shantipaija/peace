611ce3b9b1c10b5c7fe7033bf36cc2aaf6916177
Anup Raj <anupofuniverse@gmail.com>
2018-01-02 16:13:28 +0545

    remove author box from search result page

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -75,31 +75,4 @@
 
 		</footer><!-- .entry-meta -->
 	</div>
-
-	<?php if ( get_the_author_meta( 'description' ) ) :  ?>
-		<div class="post-inner-content secondary-content-box">
-	  <!-- author bio -->
-	  <div class="author-bio content-box-inner">
-
-		<!-- avatar -->
-		<div class="avatar">
-			<?php echo get_avatar( get_the_author_meta( 'ID' ) , '60' ); ?>
-		</div>
-		<!-- end avatar -->
-
-		<!-- user bio -->
-		<div class="author-bio-content">
-
-		  <h4 class="author-name"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo get_the_author_meta( 'display_name' ); ?></a></h4>
-		  <p class="author-description">
-				<?php echo get_the_author_meta( 'description' ); ?>
-		  </p>
-
-		</div><!-- end .author-bio-content -->
-
-	  </div><!-- end .author-bio  -->
-
-		</div>
-		<?php endif; ?>
-
 </article><!-- #post-## -->
