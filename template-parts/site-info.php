973be68bcb54e64e34204d663d494d627cfb96b4
shantipaija <shantipaija.com.np@gmail.com>
2018-02-19 15:39:14 +0545

    space

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -9,6 +9,6 @@
  */
 
 ?>
-	<a href="<?php echo esc_url( __( 'https://www.customwptheme.co/mastery', 'peace' ) ); ?>" title="<?php printf( __( '%s Theme', 'peace' ), 'mastery' ); ?>"><?php printf( __( '%s Theme', 'peace' ), 'mastery' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://www.customwptheme.co/peace', 'peace' ) ); ?>" title="<?php printf( __( '%s Theme', 'peace' ), 'peace' ); ?>"><?php printf( __( '%s Theme', 'peace' ), 'Peace' ); ?></a>
 	<a href="<?php echo esc_url( __( 'https://www.customwptheme.co/', 'peace' ) ); ?>" title="<?php printf( __( ' by %s', 'peace' ), 'CustomWPTheme' ); ?>"><?php printf( __( ' by %s', 'peace' ), 'CustomWPTheme' ); ?></a>
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'peace' ) ); ?>" title="<?php printf( __( 'Powered by %s', 'peace' ), 'WordPress' ); ?>"><?php printf( __( 'Powered by %s', 'peace' ), 'WordPress' ); ?></a>
